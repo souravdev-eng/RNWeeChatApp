a9dd0722aad0cd8670e9ecfd1f185fb54078d129
sourav-allmysons <sourav@in.allmysons.com>
2023-08-25 13:00:13 +0530

    Install npm package and set up aws export file

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Navigator from './src/navigation';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 export default function App() {
   return (
